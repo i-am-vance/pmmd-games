ed86c9457426b3555c62ffdb4016a9c95ec28ffd
vance <iamvancenet@gmail.com>
2022-08-31 12:14:59 +0430

    Writing the minus function

--- main.py
+++ main.py
@@ -19,4 +19,23 @@ def plus():
 	else:
 		print("The answer is wrong :/")
 
+def minus():
+	digit_one = int(input("Enter the first number :"))
+	digit_tow = int(input("Enter the second number :"))
+
+	first_number = randrange(digit_one)
+	second_number = randrange(digit_tow)
+	
+	print("%s - %s" %(first_number, second_number))
+
+	minus_r_number = first_number + second_number 
+
+	user_input = int(input("Enter the answer: "))
+
+	if user_input == plus_r_number :
+		print("The answer is correct :)")
+	else:
+		print("The answer is wrong :/")
+
+
 
